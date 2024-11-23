55140bf7e7f50c4ddc0906e2d95e4c2ddb3dac30
Matthew Garcia <gmatthew2765@gmail.com>
2024-11-23 13:27:09 -0800

    Fixed background image issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Time from './Time';
+import mountainImage from '../public/john-towner-JgOeRuGD_Y4-unsplash.jpg';
 
 function App() {
   return (
     <div className="relative">
       <div>
-        <img className="h-screen w-screen" src="../public/john-towner-JgOeRuGD_Y4-unsplash.jpg" alt="Morning Mountains" />
+        <img className="h-screen w-screen" src={mountainImage} alt="Mountains" />
         <Time />
       </div>
     </div>
